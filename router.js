e41425ae64e3b0803fa79c38dafc64e03a4aac8d
Andrew Lee <student@epicodus.com>
2015-06-08 16:24:33 -0700

    remake of bustle.com

---
+++ router.js
@@ -0,0 +1,7 @@
+Bustle.Router.map(function(){
+  this.resource('headlines', {path: '/'});
+  this.resource('worldpicpage1');
+  this.resource('worldpicpage2');
+  this.resource('worldpicpage3');
+  this.resource('worldpicpage4');
+});
